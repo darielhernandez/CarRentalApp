695b7e959bbd68ce27df8793442693da18ee6d59
darielhernandez <13dariel@gmail.com>
2022-01-03 21:23:03 -0500

    Added Challenge 2

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -1,11 +1,16 @@
 package com.careerdevs;
 
 public class Main {
-
+//challenge 1
     public static void main(String[] args) {
         String firstName= "Dariel";
         String lastName= "Hernandez";
         String fullName= firstName + " " + lastName;
-        System.out.println("this is a test");
+        System.out.println(fullName);
     }
+    //challenge 2
+    public static String createFullName(String firstName, String lastName){
+        return firstName+" "+lastName;
+    }
+
 }
