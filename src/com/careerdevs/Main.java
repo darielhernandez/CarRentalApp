5c0e856af43005ce881160cd3f5e37184369e903
darielhernandez <13dariel@gmail.com>
2022-01-03 21:12:10 -0500

    Initial commit

---
+++ src/com/careerdevs/Main.java
@@ -0,0 +1,11 @@
+package com.careerdevs;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String firstName= "Dariel";
+        String lastName= "Hernandez";
+        String fullName= firstName + " " + lastName;
+        System.out.println("this is a test");
+    }
+}
