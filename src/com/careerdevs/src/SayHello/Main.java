91434236faba0c005fb324fb66837df37e4e8719
darielhernandez <13dariel@gmail.com>
2022-01-13 16:36:47 -0500

    Say Hello

---
+++ src/com/careerdevs/src/SayHello/Main.java
@@ -0,0 +1,14 @@
+package SayHello;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+
+        String name = scanner.nextLine();
+
+        System.out.println(name);
+
+    }
+}
