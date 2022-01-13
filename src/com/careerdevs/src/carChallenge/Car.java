20cf48e8f90589a24ae91f2b971c27033c37d60e
darielhernandez <13dariel@gmail.com>
2022-01-13 16:10:52 -0500

    Car assignment Java.

---
+++ src/com/careerdevs/src/carChallenge/Car.java
@@ -0,0 +1,15 @@
+package carChallenge;
+
+public class Car {
+    public String make;
+    public String model;
+    public int mileage;
+    public float gasTankPercentage;
+
+    public Car(String make, String model, int mileage, float gasTankPercentage) {
+        this.make = make;
+        this.model = model;
+        this.mileage = mileage;
+        this.gasTankPercentage = gasTankPercentage;
+    }
+}
---
+++ src/com/careerdevs/src/com/careerdevs/Main.java
@@ -0,0 +1,7 @@
+package com.careerdevs;
+import java.security.PublicKey;
+public class Car {
+    public static void main(String[] args) {
+
+    }
+}
