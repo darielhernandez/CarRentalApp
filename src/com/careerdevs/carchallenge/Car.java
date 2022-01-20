ce2735c424f70e87172cb3b599387bf676c9b2ff
darielhernandez <13dariel@gmail.com>
2022-01-20 14:41:38 -0500

    Car challenge fully working and completed

--- src/com/careerdevs/carchallenge/Car.java
+++ src/com/careerdevs/carchallenge/Car.java
@@ -1,20 +1,25 @@
 package com.careerdevs.carchallenge;
 
 public class Car {
-    public String company;
-    public int horsepower;
-    public int speed;
+    public String make;
+    public String model;
+    public String owner;
+    public int mileage;
+    public float gasTank;
 
-    public Car(String company, int speed, int horsepower) {
-        this.company = company;
-        this.speed = speed;
-        this.horsepower= horsepower;
+    public Car(String make, String model, String owner, int mileage, float gasTank) {
+        this.make = make;
+        this.model= model;
+        this.owner= owner;
+        this.mileage= mileage;
+        this.gasTank= gasTank;
+
     }
 
+    public Car(String owner, String make, String model) {
+    }
 
+    public Car(String make, String model, int mileage, float gasTank) {
 
-    public void showCar() {
-
-//        System.out.println(company + "'s cars have a top speed of " + speed + "Km/h\nWith a whopping " + horsepower + "horsepower!");
     }
 }
--- src/com/careerdevs/carchallenge/Main.java
+++ src/com/careerdevs/carchallenge/Main.java
@@ -7,33 +7,50 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        makeACar();
-
+//        makeACar();
+        carObject();
     }
     public static void makeACar(){
         //0. create a scanner
         Scanner scanner= new Scanner(System.in);
 
         //1st user input
-        System.out.println("Enter car company: ");
-        String company= scanner.nextLine();
-//        scanner.nextLine();
+        System.out.println("What is your name?\nName");
+        String owner= scanner.nextLine();
 
-        System.out.println("Enter top speed: ");
-        int speed= scanner.nextInt();
-        scanner.nextLine();
+        System.out.println("Enter car make: ");
+        String make= scanner.nextLine();
 
-        System.out.println("Enter horsepower: ");
-        int horsepower= scanner.nextInt();
-
-        System.out.println("End of user input..");
+        System.out.println("Enter car model: ");
+        String model= scanner.nextLine();
 
         //2. car instance
-        Car car= new Car(company, speed, horsepower);
+        Car car= new Car(owner, make, model);
 
         //3. print
-        System.out.println(car);
+        System.out.println(owner + " owns a " + make +" "+ model);
 //        System.out.println("nimbus");
+    }
+    public static void carObject(){
+        //0. scanner
+        Scanner scan= new Scanner(System.in);
+        //1. user input
+        System.out.println("Make: ");
+        String make= scan.nextLine();
+
+        System.out.println("Model: ");
+        String model= scan.nextLine();
+
+        System.out.println("Mileage: ");
+        int mileage= scan.nextInt();
+
+        System.out.println("Gas Tank(0= empty, .5= half, 1= full): ");
+        float gasTank= scan.nextFloat();
+        //2. instances
+        Car car= new Car(make, model, mileage, gasTank);
+
+        //3. print
+        System.out.println("The "+ make +" "+ model + " has " + mileage + " miles on the odometer and " + gasTank + " tank of gas.");
     }
 }
 
